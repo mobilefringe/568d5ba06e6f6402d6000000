755a9158f887024b37a521da4ef5ca134bcc1c05
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-01-12 17:13:08 -0500

    Mall Maverick 1.0  /application.js

---
+++ application.js
@@ -0,0 +1,13 @@
+/*Created 2015-02-28 by CodeCloud Team*/
+
+function init(){
+    $(".scroll").click(function(e) {
+        e.preventDefault();
+        if(window.screen.width > 768){
+            $('html,body').animate( { scrollTop:$(this.hash).offset().top-85 } , 500);
+        }
+        else{
+            $('html,body').animate( { scrollTop:$(this.hash).offset().top } , 500);
+        }
+    });
+}
