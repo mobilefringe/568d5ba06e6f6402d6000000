70f84f6fe8553a2d161689d1a9290fb9e05cc4b3
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-01-15 12:45:57 -0500

    Mall Maverick 1.0  /application.js

--- application.js
+++ application.js
@@ -18,7 +18,7 @@ function init(){
     var previousScroll = 0;
     $(window).scroll(function(event){
         var scroller = $(this).scrollTop();
-        if(scroller > (previousScroll + 20) || scroller < (previousScroll - 20)){
+        if(scroller > (previousScroll + 10) || scroller < (previousScroll - 20)){
             $('#feature_insider').slideUp()
         }
         previousScroll = scroller;
