c716f48d73fe2bbd90edbd330bf08634d3814c56
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-01-15 12:42:19 -0500

    Mall Maverick 1.0  /application.js

--- application.js
+++ application.js
@@ -14,9 +14,15 @@ function init(){
         e.preventDefault();
         $('#feature_insider').slideToggle()
     });
+    
+    var previousScroll = 0;
     $(window).scroll(function(event){
-        
-        
+        var scroller = $(this).scrollTop();
+        if (scroller-senseSpeed > previousScroll){
+        $("div.button").filter(':not(:animated)').slideUp();
+        } else if (scroller+senseSpeed < previousScroll) {
+        $("div.button").filter(':not(:animated)').slideDown();
         }
+        previousScroll = scroller;
     });
 }
