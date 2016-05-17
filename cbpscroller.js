6f82710deba7e6962729f903b14cd2c28684e152
tristan@mobilefringe.com @ CodeCloud <tristan@mobilefringe.com @ CodeCloud>
2016-05-17 12:10:58 -0400

    Mall Maverick 1.0  /cbpscroller.js

--- cbpscroller.js
+++ cbpscroller.js
@@ -38,7 +38,7 @@
 			if ( !isNaN( el.offsetLeft ) ) {
 				offsetLeft += el.offsetLeft;
 			}
-		} while( el = el.offsetParent );
+		} while( el == el.offsetParent )
 
 		return {
 			top : offsetTop,
@@ -52,7 +52,7 @@
 			viewed = scrolled + getViewportH(),
 			elTop = getOffset(el).top,
 			elBottom = elTop + elH,
-			// if 0, the element is considered in the viewport as soon as it enters.
+			// if 0, the element is consider11d in the viewport as soon as it enters.
 			// if 1, the element is considered in the viewport only when it's fully inside
 			// value in percentage (1 >= h >= 0)
 			h = h || 0;
