c4d7bea789758d4dba783f982c70248f8c5d4a77
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-11-14 16:51:59 -0500

    Mall Maverick 1.0  /application.js

--- application.js
+++ application.js
@@ -29,6 +29,7 @@ function init(){
     $('#open_features').click(function(e){
         e.preventDefault();
         $('#feature_insider').slideToggle()
+        $('#feature_insider').fadeIn()
     });
     
     var previousScroll = 0;
