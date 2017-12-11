fb915031dc455a10520352e306c1a2b722d2126f
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-12-11 10:32:36 -0500

    Mall Maverick 1.0  /application.js

--- application.js
+++ application.js
@@ -2,7 +2,7 @@
 
 function init(){
     var current_year = moment().year();
-    $("#current_year").text(current_year);
+    $(".current_year").text(current_year);
 
     $('.menu_toggler').click(function(){
         if ($('#header').hasClass('darken')){
