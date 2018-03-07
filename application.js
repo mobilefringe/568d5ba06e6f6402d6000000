f7f41714501f9a52e4a25a2245a52f3ef5cddf38
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-07 11:18:12 -0500

    Mall Maverick 1.0  /application.js

--- application.js
+++ application.js
@@ -1,9 +1,12 @@
 /*Created 2015-02-28 by CodeCloud Team*/
-
-function init(){
+$(document).ready(function(){
+    //dynamically changing copyright year
     var current_year = moment().year();
     $(".current_year").text(current_year);
+});
 
+function init(){
+    
     $('.menu_toggler').click(function(){
         if ($('#header').hasClass('darken')){
             $('#header').addClass('lighten');
@@ -123,7 +126,177 @@ function renderGeneral(container, template, collection){
     });
     $(container).html(item_rendered.join(''));
 }
-        
+
+function renderBanner(banner_template,home_banner,banners){
+    var item_list = [];
+    var item_rendered = [];
+    var banner_template_html = $(banner_template).html();
+    Mustache.parse(banner_template_html);   // optional, speeds up future uses
+    $.each( banners , function( key, val ) {
+        today = new Date();
+        start = new Date (val.start_date);
+        start.setDate(start.getDate());
+        if(val.url == "" || val.url === null){
+            val.css = "style=cursor:default;";
+            val.noLink = "return false";
+        }
+        if (start <= today){
+            if (val.end_date){
+                end = new Date (val.end_date);
+                end.setDate(end.getDate() + 1);
+                if (end >= today){
+                    item_list.push(val);  
+                }
+            } else {
+                item_list.push(val);
+            }
+        }
+    });
+
+    $.each( item_list , function( key, val ) {
+        var repo_rendered = Mustache.render(banner_template_html,val);
+        item_rendered.push(repo_rendered);
+    });
+    $(home_banner).html(item_rendered.join(''));
+}
+
 function fadeContent(){
     $('#feature_insider .row').fadeIn();
+}
+
+function renderPosts(container, template, collection){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    var counter = 1;
+    Mustache.parse(template_html);   // optional, speeds up future uses
+    
+    $.each( collection , function( key, val ) {
+        if (val.image_url.indexOf('missing.png') > -1) {
+            val.post_image = "//codecloud.cdn.speedyrails.net/sites/59c082786e6f6462ee1d0000/image/png/1507232968000/Group 10.png";
+        } else {
+            val.post_image = val.image_url;
+        }
+        
+        val.title = truncateWords(val.title,7);
+        if(val.body.length > 100){
+            val.description_short = val.body.substring(0, 100) + "...";
+        } else {
+            val.description_short = val.body;
+        }
+        
+        val.description_short = val.description_short.replace("&amp;", "&");
+        
+        var published_on = moment(val.publish_date).tz(getPropertyTimeZone());
+        val.publish_date = published_on.format("MMM D, YYYY");
+        
+        //get first tag 
+        if(val.tag != null && val.tag !== undefined) {
+            val.main_tag = val.tag[0];
+            // console.log("main tag", val.main_tag);
+            val.show_tag = "display:inline-block";
+        }
+        else {
+            val.show_tag = "display:none"
+        }
+        
+        //get the tag that is capitalized
+        // if(val.tag != null && val.tag !== undefined) {
+        //     console.log(val.tag);
+        //     $.each( val.tag , function( key, tag ) {
+               
+                
+        //         if(tag[0] === tag[0].toUpperCase()){
+        //             val.main_tag = tag;
+        //         }
+        //     })
+        //     console.log("main tag", val.main_tag);
+        // }
+        
+        val.counter = counter;
+        
+        var rendered = Mustache.render(template_html,val);
+        item_rendered.push(rendered);
+        counter = counter + 1;
+        // item_list.push(val);
+    });
+    
+    // $(container).show();
+    $(container).html(item_rendered.join(''));
+}
+function renderPostDetails(container, template, collection, blog_posts){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    $.each(collection , function( key, val ) {
+        if (val.image_url.indexOf('missing.png') > -1) {
+            val.post_image = "//codecloud.cdn.speedyrails.net/sites/57f7f01f6e6f647835890000/image/png/1461352407000/HallifaxLogo.png";
+        } else {
+            val.post_image = val.image_url;
+        }
+        
+        if(val.body.length > 150){
+            val.description_short = val.body.substring(0,150) + "...";
+        }
+        else{
+            val.description_short = val.body;
+        }
+
+        var blog_list = [];
+        $.each(blog_posts, function(key, val) {
+            var slug = val.slug;
+            blog_list.push(val.slug);
+        });
+        var current_slug = val.slug;
+        var index = blog_list.indexOf(current_slug);
+        if(index >= 0 && index < blog_list.length){
+          var next_slug = blog_list[index + 1];
+            if(next_slug != undefined || next_slug != null){
+                val.next_post = "/blog/" + next_slug;
+                val.next_show = "display: block";
+            } else {
+                val.next_show = "display: none";
+            }
+        }
+        if(index >= 0 && index < blog_list.length){
+            var prev_slug = blog_list[index - 1];
+            if(prev_slug != undefined || prev_slug != null){
+                val.prev_post = "/blog/" + prev_slug;
+                val.prev_show = "display: block";
+            } else {
+                val.prev_show = "display: none";
+            }
+        }
+        var published_on = moment(val.publish_date).tz(getPropertyTimeZone());
+        val.publish_date = published_on.format("MMM D, YYYY");
+        
+        if(val.tag != null && val.tag !== undefined) {
+            val.main_tag = val.tag[0];
+            // console.log("main tag", val.main_tag);
+            val.show_tag = "display:inline-block";
+        }
+        else {
+            val.show_tag = "display:none"
+        }
+        
+        val.twitter_title = val.title + " via @shopHSC";
+        
+        var rendered = Mustache.render(template_html,val);
+        item_rendered.push(rendered);
+    });
+    
+    $(container).html(item_rendered.join(''));
+}
+
+function truncateWords(str, no_words) {
+    // return str.split(" ").splice(0,no_words).join(" ")+"...";
+
+    str = str.trim().split(" ");
+    if(str.length > no_words) {
+        return str.splice(0, no_words).join(" ")+ "...";
+    }
+    else {
+        return str.splice(0, no_words).join(" ");
+    }
+    
 }
