c8b823490c5db12acbab5298054207114b391688
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-27 17:59:40 -0400

    Mall Maverick 1.0  /home.js

--- home.js
+++ home.js
@@ -17,30 +17,30 @@ $(document).ready(function(){
     });
     
     
-    $('#subForm').submit(function(e){
-        e.preventDefault();
-        $('.submit_btn').prop('disabled', true);
-        if($("#agree_terms").is(':checked')){
-            $.getJSON(
-                this.action + "?callback=?",
-                $(this).serialize(),
-                function (data) {
-                    if (data.Status === 400) {
-                        alert("An error occured while processing your request. Please try again later.");
-                    } else { // 200
-                        $("#success_subscribe").fadeIn();
-                        $('#success_subscribe').delay(2000).fadeOut();
-                        $('#subForm').trigger('reset');
-                        $('.submit_btn').prop('disabled', false);
-                }
-            });
-        }
-        else{
-            $("#agree_terms").focus();
-            alert("Please agree to receive newsletter before continuing.")
-            $('.submit_btn').prop('disabled', false);
-        }
-    });
+    // $('#subForm').submit(function(e){
+    //     e.preventDefault();
+    //     $('.submit_btn').prop('disabled', true);
+    //     if($("#agree_terms").is(':checked')){
+    //         $.getJSON(
+    //             this.action + "?callback=?",
+    //             $(this).serialize(),
+    //             function (data) {
+    //                 if (data.Status === 400) {
+    //                     alert("An error occured while processing your request. Please try again later.");
+    //                 } else { // 200
+    //                     $("#success_subscribe").fadeIn();
+    //                     $('#success_subscribe').delay(2000).fadeOut();
+    //                     $('#subForm').trigger('reset');
+    //                     $('.submit_btn').prop('disabled', false);
+    //             }
+    //         });
+    //     }
+    //     else{
+    //         $("#agree_terms").focus();
+    //         alert("Please agree to receive newsletter before continuing.")
+    //         $('.submit_btn').prop('disabled', false);
+    //     }
+    // });
     
 // 	jQuery('.dynpost-left').addClass("hiddenAnimate").viewportChecker({
 // 	    classToAdd: 'visibleAnimate animated bounceInLeft',
