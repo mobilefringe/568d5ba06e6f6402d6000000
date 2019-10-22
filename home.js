27a3df3e1ab70a66c0c6f0f4a6af268afedc10ec
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-10-22 15:38:15 -0400

    Mall Maverick 1.0  /home.js

--- home.js
+++ home.js
@@ -67,18 +67,18 @@ function renderAll () {
     
     var video_repo = getRepoDetailsByName('Banner Video');
     console.log("video_repo", video_repo)
-    if( video_repo && video_repo.images){
-        video_repo = video_repo.images[0]
-        var video_banner = {};
-        video_banner.name = "A COMPLETE MANAGEMENT SYSTEM FOR ALL YOUR DIGITAL CHANNELS"
-        video_banner.description = "Learn More";
-        video_banner.image_url= video_repo.image_url//"https://www.mallmaverick.com/system/site_images/photos/000/045/539/original/GirlWalkingMM1.mp4?1541450822";
-        video_banner.url = "/";
-        video_banner.isVideo = true;
-        video_banner.className="video_banner"
-        video_banner.start_date = null;
-        banners.unshift(video_banner);
-    }
+    // if( video_repo && video_repo.images){
+    //     video_repo = video_repo.images[0]
+    //     var video_banner = {};
+    //     video_banner.name = "A COMPLETE MANAGEMENT SYSTEM FOR ALL YOUR DIGITAL CHANNELS"
+    //     video_banner.description = "Learn More";
+    //     video_banner.image_url= video_repo.image_url//"https://www.mallmaverick.com/system/site_images/photos/000/045/539/original/GirlWalkingMM1.mp4?1541450822";
+    //     video_banner.url = "/";
+    //     video_banner.isVideo = true;
+    //     video_banner.className="video_banner"
+    //     video_banner.start_date = null;
+    //     banners.unshift(video_banner);
+    // }
     console.log("banners", banners)
     renderBanner('#banner_template','#home_banner', banners);
      
